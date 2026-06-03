aea06e580379396d0cd33b8bf7e0a6c4c6a43601
Antoine-Chen <antoinecoc2408@gmail.com>
2026-06-03 10:49:16 +0200

    ajout achi + router dom (routes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-export default function App() {
-    return (
-        <>
+// import {Route, Routes} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.tsx";
+// import Home from "./pages/Home.tsx";
+// import Navbar from "./components/Navbar.tsx";
+// import Footer from "./components/footer/Footer.tsx";
+// import About from "./pages/About.tsx";
+// import Contact from "./components/form/Contact.tsx";
+// import Projets from "./pages/Projets.tsx";
 
-        </>
-    );
+function App() {
+    return (
+        <div className="overflow-x-hidden">
+            <ScrollToTop />
+            {/*<Navbar/>*/}
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<Home />} />*/}
+            {/*    <Route path="/about" element={<About/> } />*/}
+            {/*    <Route path="/projets" element={<Projets/>} />*/}
+            {/*    <Route path="/contact" element={<Contact />} />*/}
+
+            {/*</Routes>*/}
+            {/*<Footer/>*/}
+        </div>
+    )
 }
+
+export default App
